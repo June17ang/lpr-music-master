2c2ca681e899430d0b76ce3886cefb0ff396e3c0
June Ang <June Ang>
2020-09-07 22:06:38 +0800

    Format code in files

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
-import Artist from './Artists';
+import React, { Component } from "react";
+import Artist from "./Artists";
 
-const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+const API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
 
 class App extends Component {
-  state = { artistQuery: '', artist: null, tracks:[] };
+  state = { artistQuery: "", artist: null, tracks: [] };
 
-  updateArtistQuery = event => {
+  updateArtistQuery = (event) => {
     this.setState({ artistQuery: event.target.value });
-  }
+  };
 
   searchArtist = () => {
-    console.log('this.state', this.state)
+    console.log("this.state", this.state);
 
     fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
-      .then(response => response.json())
-      .then(json => {
-        console.log('json', json);
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("json", json);
         if (json.artists.total > 0) {
           const artist = json.artists.items[0];
 
           this.setState({ artist });
 
           fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
-            .then(response => response.json())
-            .then(json => this.setState({ tracks: json.tracks }))
-            .catch(error => alert(error.message));
+            .then((response) => response.json())
+            .then((json) => this.setState({ tracks: json.tracks }))
+            .catch((error) => alert(error.message));
         }
       })
-      .catch(error => alert(error.message));
-  }
+      .catch((error) => alert(error.message));
+  };
 
-  handleKeyPress = event => {
-    if (event.key === 'Enter') {
+  handleKeyPress = (event) => {
+    if (event.key === "Enter") {
       this.searchArtist();
     }
-  }
+  };
 
   render() {
     return (
       <div>
         <h2>Music Master</h2>
-        <input 
-          onChange={ this.updateArtistQuery }
-          onKeyPress={ this.handleKeyPress } 
-          placeholder='Search for an Artist' 
+        <input
+          onChange={this.updateArtistQuery}
+          onKeyPress={this.handleKeyPress}
+          placeholder="Search for an Artist"
         />
-        <button onClick={ this.searchArtist }>Search</button>
+        <button onClick={this.searchArtist}>Search</button>
         <Artist artist={this.state.artist} />
       </div>
-    )
+    );
   }
 }
 
