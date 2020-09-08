1cecdcd8c146f3fe0e90df9713bf1b1f8329cba2
June Ang <June Ang>
2020-09-08 09:22:37 +0800

    Add track play and pause icon

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 
 class Tracks extends Component {
-  state = { playing: false, audio: null, playingPreviewUrl: null };
+  state = {
+    playing: false,
+    audio: null,
+    playingPreviewUrl: null,
+  };
 
   //double => will be callback
   playAudio = (previewUrl) => () => {
@@ -21,6 +25,21 @@ class Tracks extends Component {
       }
     }
   };
+
+  trackIcon = (track) => {
+    if (!track.preview_url) {
+      return <span>N/A</span>;
+    }
+
+    if (
+      this.state.playing &&
+      this.state.playingPreviewUrl === track.preview_url
+    ) {
+      return <span>||</span>;
+    }
+    return <span>&#9654;</span>;
+  };
+
   render() {
     const { tracks } = this.props;
 
@@ -30,9 +49,18 @@ class Tracks extends Component {
           const { id, name, album, preview_url } = track;
 
           return (
-            <div key={id} onClick={this.playAudio(preview_url)}>
-              <img src={album.images[0].url} alt="track-image" />
-              <p>{name}</p>
+            <div
+              key={id}
+              onClick={this.playAudio(preview_url)}
+              className="track"
+            >
+              <img
+                src={album.images[0].url}
+                alt="track-image"
+                className="track-image"
+              />
+              <p className="track-text">{name}</p>
+              <p className="track-icon">{this.trackIcon(track)}</p>
             </div>
           );
         })}
